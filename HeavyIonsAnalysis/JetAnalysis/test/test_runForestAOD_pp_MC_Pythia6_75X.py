07124ba2d77cf151d84c0fa3effec7f55dc4bb87
JelovXCMS <peng43@purdue.edu>
2017-04-26 18:22:43 -0400

    test

--- HeavyIonsAnalysis/JetAnalysis/test/test_runForestAOD_pp_MC_Pythia6_75X.py
+++ HeavyIonsAnalysis/JetAnalysis/test/test_runForestAOD_pp_MC_Pythia6_75X.py
@@ -42,7 +42,7 @@ process.source = cms.Source("PoolSource",
 
 # Number of events we want to process, -1 = all events
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000))
+    input = cms.untracked.int32(200))
 #    input = cms.untracked.int32(-1))
 
 
